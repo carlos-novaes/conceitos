bd89df9c32f4589313f8fa62ba245a5864e33217
Carlos Novaes <carlos.novaes@elevaeducacao.com.br>
2021-01-10 23:19:33 -0300

    adicionando estados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 const App = () => {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  const handleAddProject = () => {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  };
+
   return (
     <>
-      <Header title='Homepage'>
-        <p>Test child</p>
-      </Header>
-      <Header title='Projects' />
+      <Header title='Homepage' />
+
+      <ul>
+        {projects.map((project, index) => (
+          <li key={index}>{project}</li>
+        ))}
+      </ul>
+
+      <button type='button' onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 };
